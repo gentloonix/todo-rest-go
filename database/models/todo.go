3c146114775adf57c7174698f69cd8c834e4f0cd
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-12 16:56:27 +0800

    tag

--- database/models/todo.go
+++ database/models/todo.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type TODO struct {
 	gorm.Model
-	Id          uint64 `gorm:"primaryKey"`
-	UserId      uint64
-	Name        string
-	Description string
-	DueDate     uint64
-	Status      bool
+	Id          uint64 `json:"id" gorm:"primaryKey"`
+	UserId      uint64 `json:"user_id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	DueDate     uint64 `json:"due_date"`
+	Status      bool   `json:"status"`
 }

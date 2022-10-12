ef7694fe348bfdcd443c9c515e4dae7f8ecf2e72
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-12 14:02:16 +0800

    gin and gorn test

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -23,24 +25,36 @@ type TODO struct {
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	sqlDB, err := db.DB()
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := sqlDB.Close(); err != nil {
-			log.Println(err)
+	go func() {
+		db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		sqlDB, err := db.DB()
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			if err := sqlDB.Close(); err != nil {
+				log.Println(err)
+			}
+		}()
+
+		if err := db.AutoMigrate(&User{}); err != nil {
+			panic(err)
+		}
+		if err := db.AutoMigrate(&TODO{}); err != nil {
+			panic(err)
 		}
 	}()
 
-	if err := db.AutoMigrate(&User{}); err != nil {
-		panic(err)
-	}
-	if err := db.AutoMigrate(&TODO{}); err != nil {
-		panic(err)
-	}
+	go func() {
+		r := gin.Default()
+
+		r.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+		})
+
+		r.Run()
+	}()
 }

64c20900dfe7f938496863b5215327c46b8a0ef8
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-12 13:47:07 +0800

    base

--- main.go
+++ main.go
@@ -1,2 +1,33 @@
 package main
 
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Id uint64
+}
+
+type TODO struct {
+	gorm.Model
+	UserId      uint64
+	Name        string
+	Description string
+	DueDate     uint64
+	Status      bool
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	defer sqlDB.Close()
+
+}

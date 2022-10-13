13a1d0d2a7c1e0214b6b98e3a8e30a7de6d69ff4
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-13 10:11:41 +0800

    main() cleanup

--- main.go
+++ main.go
@@ -11,15 +11,18 @@ import (
 )
 
 func main() {
+	// Async submodules initialize
 	var wg sync.WaitGroup
 
 	wg.Add(1)
 	database.Initialize(&wg)
 
+	// Await SIGINT / SIGTERM
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 
+	// Await async submodules exit
 	log.Println("exiting...")
 
 	database.Close <- struct{}{}

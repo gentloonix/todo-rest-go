3f366653a25cc97fe911e1b950377a54ee79e97a
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-13 14:44:23 +0800

    json ignore empty fields

--- models/todo.go
+++ models/todo.go
@@ -6,8 +6,8 @@ type TODO struct {
 	gorm.Model
 	Id          uint64 `json:"id" gorm:"primaryKey"`
 	UserId      User   `json:"user_id" gorm:"foreignKey:Id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	DueDate     int64  `json:"due_date"`
-	Status      bool   `json:"status"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	DueDate     int64  `json:"due_date,omitempty"`
+	Status      bool   `json:"status,omitempty"`
 }

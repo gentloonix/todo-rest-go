79b6b7dc92d02b69761a28454f7c659d3a3acaea
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-14 15:36:23 +0800

    test value

--- models/api_utils.go
+++ models/api_utils.go
@@ -1,17 +1,23 @@
 package models
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func ApiGet[T IModels](c *gin.Context) {
+	log.Printf("%#v\n", c.Request.URL.Query())
 }
 
 func ApiPost[T IModels](c *gin.Context) {
+	log.Printf("%#v\n", c.Request.URL.Query())
 }
 
 func ApiPut[T IModels](c *gin.Context) {
+	log.Printf("%#v\n", c.Request.URL.Query())
 }
 
 func ApiDelete[T IModels](c *gin.Context) {
+	log.Printf("%#v\n", c.Request.URL.Query())
 }

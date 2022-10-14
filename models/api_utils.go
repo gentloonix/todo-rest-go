311acca6e10a70f2685af7290859cc1d0292c6e7
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-14 19:30:14 +0800

    generic api logic

--- models/api_utils.go
+++ models/api_utils.go
@@ -6,23 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func parseApiQuery(c *gin.Context) (where map[string]interface{}, order string, update map[string]interface{}) {
+func parseApiQuery(c *gin.Context) (where map[string]interface{}, order string, updates map[string]interface{}) {
 	where_tmp := c.Request.URL.Query()
-	order_tmp, ok := where["sortBy"].([]string)
-	delete(where, "sortBy")
+	order_tmp, ok := where_tmp["sortBy"]
 	if ok {
+		delete(where, "sortBy")
 		if len(order_tmp) != 0 {
 			order = order_tmp[0]
 		}
 	}
-	update_tmp_slice, ok := where["replaceWith"].([]string)
-	delete(where, "replaceWith")
+	updates_tmp_slice, ok := where_tmp["replaceWith"]
 	if ok {
-		if len(update_tmp_slice) != 0 {
-			update_tmp := update_tmp_slice[0]
-			update = make(map[string]interface{})
-			if err := json.Unmarshal([]byte(update_tmp), &update); err != nil {
-				update = nil
+		delete(where, "replaceWith")
+		if len(updates_tmp_slice) != 0 {
+			updates_tmp := updates_tmp_slice[0]
+			updates = make(map[string]interface{})
+			if err := json.Unmarshal([]byte(updates_tmp), &updates); err != nil {
+				updates = nil
 			}
 		}
 	}
@@ -30,16 +30,19 @@ func parseApiQuery(c *gin.Context) (where map[string]interface{}, order string, 
 	for k, v := range where_tmp {
 		where[k] = v
 	}
-	return where, order, update
+	return where, order, updates
 }
 
 func ApiGet[T IModels](c *gin.Context) {
-	where, order := parseApiQuery(c)
+	where, order, _ := parseApiQuery(c)
 	objs, err := OrmQuery[T](where, order)
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 	} else {
-		c.JSON(200, gin.H{"result": objs})
+		c.JSON(200, gin.H{
+			"result": objs,
+			"msg":    "ok",
+		})
 	}
 }
 
@@ -48,21 +51,19 @@ func ApiPost[T IModels](c *gin.Context) {
 }
 
 func ApiPut[T IModels](c *gin.Context) {
-	where, _ := parseApiQuery(c)
-	objs, err := OrmUpdate[T](where)
-	if err != nil {
+	where, _, values := parseApiQuery(c)
+	if err := OrmUpdate[T](where, values); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 	} else {
-		c.JSON(200, gin.H{"result": objs})
+		c.JSON(200, gin.H{"msg": "ok"})
 	}
 }
 
 func ApiDelete[T IModels](c *gin.Context) {
-	where, _ := parseApiQuery(c)
-	objs, err := OrmDelete[T](where)
-	if err != nil {
+	where, _, _ := parseApiQuery(c)
+	if err := OrmDelete[T](where); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 	} else {
-		c.JSON(200, gin.H{"result": objs})
+		c.JSON(200, gin.H{"msg": "ok"})
 	}
 }

8627447c2b634cdb84b77fccb5ad3cab084ed8da
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-14 11:55:28 +0800

    rename

---
+++ orm/orm.go
@@ -0,0 +1,57 @@
+package orm
+
+import (
+	"log"
+	"sync"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	Close chan struct{} = make(chan struct{}, 1)
+	DB    *gorm.DB      = nil
+)
+
+func Initialize(wg *sync.WaitGroup, dsn string, autoMigrate func(*gorm.DB)) {
+	if wg == nil {
+		log.Println("orm::Initialize: nil wg")
+		return
+	}
+	if DB != nil {
+		log.Println("orm::Initialize: ignoring")
+		return
+	}
+
+	go func() {
+		defer wg.Done()
+
+		defer func() {
+			log.Println("orm: exit")
+		}()
+
+		var err error
+		DB, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{
+			QueryFields: true,
+		})
+		if err != nil {
+			panic(err)
+		}
+		sqlDB, err := DB.DB()
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			if err := sqlDB.Close(); err != nil {
+				log.Println(err)
+			}
+
+			DB = nil
+		}()
+
+		autoMigrate(DB)
+
+		log.Println("orm: running")
+		<-Close
+	}()
+}

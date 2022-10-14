a69fcf871504383e3a34c08570eab5479692dc0a
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-14 11:59:36 +0800

    models

--- models/orm_utils.go
+++ models/orm_utils.go
@@ -1,14 +1,14 @@
-package database_models
+package models
 
 import (
-	"main/database"
+	"main/orm"
 
 	"gorm.io/gorm"
 )
 
 func AutoMigrate(db *gorm.DB) {
 	if db == nil {
-		panic("database_models::AutoMigrate: nil db")
+		panic("models::AutoMigrate: nil db")
 	}
 	if err := db.AutoMigrate(&User{}); err != nil {
 		panic(err)
@@ -19,12 +19,12 @@ func AutoMigrate(db *gorm.DB) {
 }
 
 func Create[T any](objs []T) error {
-	return database.DB.Create(objs).Error
+	return orm.DB.Create(objs).Error
 }
 
 func Query[T any](values map[string]interface{}) ([]T, error) {
 	var objs []T
-	if err := database.DB.Where(values).Find(&objs).Error; err != nil {
+	if err := orm.DB.Where(values).Find(&objs).Error; err != nil {
 		return nil, err
 	} else {
 		return objs, nil
@@ -33,10 +33,10 @@ func Query[T any](values map[string]interface{}) ([]T, error) {
 
 func Update[T any](ids []int, values map[string]interface{}) error {
 	var obj T
-	return database.DB.Model(&obj).Where("id IN ?", ids).Updates(values).Error
+	return orm.DB.Model(&obj).Where("id IN ?", ids).Updates(values).Error
 }
 
 func Delete[T any](ids []int) error {
 	var obj T
-	return database.DB.Where("id IN ?", ids).Delete(&obj).Error
+	return orm.DB.Where("id IN ?", ids).Delete(&obj).Error
 }
--- models/todo.go
+++ models/todo.go
@@ -1,4 +1,4 @@
-package database_models
+package models
 
 import "gorm.io/gorm"
 

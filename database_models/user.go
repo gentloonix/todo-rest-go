27d0213f7ae36b95bf8f67500ff7459b957c0417
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-14 11:39:29 +0800

    user fields

--- database_models/user.go
+++ database_models/user.go
@@ -4,5 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Id uint64 `json:"id" gorm:"primaryKey"`
+	Id             uint64 `json:"id" gorm:"primaryKey"`
+	Email          string `json:"email"`
+	Argon2IDSecret string `json:"secret"`
 }
